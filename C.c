f8f158f3ec0128ea380c267f6640a5f753153832
sty0507 <styoung0507@gmail.com>
2021-11-21 22:44:14 +0900

    11/21 일부 구현2

--- C.c
+++ C.c
@@ -6,74 +6,85 @@
 // 2) name이 제대로 안들어감-전부 NULL로 취급. 
   
 char a[10]={0,0,0,0,0,0,0,0,0,0};
-void Register(char a[]);
-void Show(char a[]);
-void Out();
+void Register(char r[]);
+void Show(char r[]);
+void Out(char r[]);
+void Food();
 int main(){
 	int i, j;
    int n;
-   printf("1.등록 2.자리보기 3. 자리해제 4.프로그램 종료\n");
-   scanf("%d", &n);
-  	for(i=0; ;i++){
-  		switch(n){
-      		case 1:
-         		Register(a);
-      		case 2:
-         		Show(a);
-      		case 3:
-     	    	Out();
-      		case 4:
-         		return 0;
-   }
-	  } 
-   
+   do{
+   		printf("1.등록 2.자리보기 3. 자리해제 4.음식주문 5.프로그램 종료\n");
+   		n = 0;
+   		scanf("%d", &n);
+  			switch(n){
+      			case 1:
+         			Register(a);
+         			break;
+	      		case 2:
+	         		Show(a);
+	         		break;
+	      		case 3:
+	     	    	Out(a);
+	     	    	break;
+	     	    case 4:
+	     	    	Food();
+	     	    	break;
+	      		case 5:
+	         		return 0;
+	   }
+	}while(1);
 }
 void Register(char r[]){
    	int desk=0, i, j;
    	char name[20];
-   	printf("자리와 이름을 입력하세요\n");
+   	printf("자리와 이름을 입력하세요(띄워쓰기 금지)\n");
    	scanf("%d %s", &desk, &name);
    	
    	if(desk==0 || desk >10){
    		printf("Error\n");
    		return;
-   }
-   
-   	if(isdigit(name)!=0){
-   		printf("숫자를 입력하셨습니다.\n");
-   		return;
-	   }
-	   
-   	int result = Search(desk);
+   	}
    	
-   	if(result){
-   		return;
+   	int result = Search(desk);//기존에 자리에 사람이 있는지 비교 
+   	if(result==1){
+   		return; 
+   	}
+   	else{
+   		a[(desk-1)] = name;
+		printf("성공적으로 등록이 되었습니다.\n");
+		return;
    }
-	a[(desk-1)] = name;
-	printf("성공적으로 등록이 되었습니다.\n");
-	return;
+	
 }
 
 void Show(char r[]){
-   int i;
+   	int i;
    	printf("===========================================================================\n");
 	printf("    1      2      3      4      5      6      7      8      9      10\n");
 	   printf("---------------------------------------------------------------------------\n");
-   	for(i=0; i<10;i++){
+  	for(i=0; i<10;i++){
    		if(r[i]==0)
    			printf("   NULL");
    		else
-   			printf("%5s", r[i]);
+   			printf("%s", r[i]);
    }
    printf("\n===========================================================================\n");
 }
 
-void Out(){
-   
+void Out(char r[]){
+   int s;
+   printf("자리를 입력해주세요.\n");
+   scanf("%d", &s);
+   r[s-1] == 0;
+   return;
 }
 
 int Search(int desk){
-   if(a[(desk-1)==0])
-   	return 1;
+   	if(a[(desk-1)==0])
+   		return 1;
    	else return -1;
+}
+void Food(){
+	
 }
