b05c7ac7807de19056b2f96456b79975e7a323d3
sty0507 <sty0507@gmail.com>
2021-11-26 22:33:10 +0900

    11/26 링크드 리스트가 좋을 듯

--- C.c
+++ C.c
@@ -2,78 +2,128 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#define NUM 10
+
 //문제점 1) show 함수가 그냥 바로 나옴 
 // 2) name이 제대로 안들어감-전부 NULL로 취급. 
+
+struct Person{
+	char name[10];
+	char li[1];
+};
   
-char a[10]={0,0,0,0,0,0,0,0,0,0};
-void Register(char a[]);
-void Show(char a[]);
-void Out();
+char a[10];
+int k = 0;
+
+void Register(struct Person r[]);
+void Show(struct Person r[]);
+void Out(struct Person r[]);
+void Food();
+
+
 int main(){
 	int i, j;
-   int n;
-   printf("1.등록 2.자리보기 3. 자리해제 4.프로그램 종료\n");
-   scanf("%d", &n);
-  	for(i=0; ;i++){
-  		switch(n){
-      		case 1:
-         		Register(a);
-      		case 2:
-         		Show(a);
-      		case 3:
-     	    	Out();
-      		case 4:
-         		return 0;
-   }
-	  } 
-   
+   	int n;
+   	while(1){
+   		printf("1.등록 2.자리보기 3. 자리해제 4.음식주문 5.프로그램 종료\n");
+   		n = 0;
+   		scanf("%d", &n);
+  			switch(n){
+      			case 1:
+         			Register(a);
+         			break;
+	      		case 2:
+	         		Show(a);
+	         		break;
+	      		case 3:
+	     	    	Out(a);
+	     	    	break;
+	     	    case 4:
+	     	    	Food();
+	     	    	break;
+	      		case 5:
+	         		return 0;
+	   }
+	}
 }
-void Register(char r[]){
-   	int desk=0, i, j;
+
+
+void Register(struct Person r[]){
+   	int i, j, desk=0;
    	char name[20];
-   	printf("자리와 이름을 입력하세요\n");
-   	scanf("%d %s", &desk, &name);
+   	printf("자리를 입력하세요(숫자만)\n");
+   	scanf("%d", &desk);
+   	printf("이름을 입력하세요(띄어쓰기 금지, 글자만)\n");
+   	scanf("%s", &name);
    	
-   	if(desk==0 || desk >10){
+   	
+   	//에러 확인 코드 
+   	if(desk==0  || desk >10){
    		printf("Error\n");
    		return;
+   	}
+	
+   	//기존에 자리에 사람이 있는지 비교
+   	int result = Search(desk); 
+   	if(result==1){
+   		return; 
+   	}
+   	else{
+   		strcpy(r[k].name, name);
+   		strcpy(r[k].li, "1");
+		printf("성공적으로 등록이 되었습니다.\n");
+		k++;
+		return;
    }
-   
-   	if(isdigit(name)!=0){
-   		printf("숫자를 입력하셨습니다.\n");
-   		return;
-	   }
-	   
-   	int result = Search(desk);
-   	
-   	if(result){
-   		return;
-   }
-	a[(desk-1)] = name;
-	printf("성공적으로 등록이 되었습니다.\n");
-	return;
+	
 }
 
-void Show(char r[]){
-   int i;
-   	printf("===========================================================================\n");
-	printf("    1      2      3      4      5      6      7      8      9      10\n");
-	   printf("---------------------------------------------------------------------------\n");
-   	for(i=0; i<10;i++){
-   		if(r[i]==0)
-   			printf("   NULL");
-   		else
-   			printf("%5s", r[i]);
-   }
-   printf("\n===========================================================================\n");
+void Show(struct Person r[]){
+   	int j;
+//   	printf("===========================================================================\n");
+//	printf("    1      2      3      4      5      6      7      8      9      10\n");
+//	printf("---------------------------------------------------------------------------\n");
+//  	for(j=0; j<10;j++){
+//   		if(r[j].li == "0")
+//   			printf("   NULL");
+//   		else
+//   			printf("%s", r[j].name);
+//   }
+//   printf("\n===========================================================================\n");
+
+	for(j=0; j<10; j++){
+		printf("%d번 자리 : %s\n", j+1, r[j].name);
+	}
 }
 
-void Out(){
-   
+void Out(struct Person r[]){
+   int s;
+   printf("자리를 입력해주세요.\n");
+   scanf("%d", &s);
+   r[s-1].name == "NULL";
+   return;
 }
 
 int Search(int desk){
-   if(a[(desk-1)==0])
-   	return 1;
+   	if(a[(desk-1)==0])
+   		return 1;
    	else return -1;
+}
+
+
+void Food(){
+	do{
+		int f=0;
+		printf("1.라면 2.음료 3.과자");
+		scanf("%d", &f);
+		switch(f){
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+		}
+	}while(1);
+	
 }
---
+++ test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+struct Person{
+	char name[10];
+	int li[10];
+};
+
+int main()
+{
+	
+	
+}
