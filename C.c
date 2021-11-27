17e562b4e297290103308e8b7cbbdabb129b8b9e
sty0507 <sty0507@gmail.com>
2021-11-27 19:10:24 +0900

    11/27 거의 다함(데이터 I/O)

--- C.c
+++ C.c
@@ -2,30 +2,31 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#define NUM 10
-
-//문제점 1) show 함수가 그냥 바로 나옴 
-// 2) name이 제대로 안들어감-전부 NULL로 취급. 
 
 struct Person{
 	char name[10];
-	char li[1];
+	char li[2];
+	char f[5];
 };
-  
+
 char a[10];
-int k = 0;
+int k = 0; 
 
 void Register(struct Person r[]);
 void Show(struct Person r[]);
 void Out(struct Person r[]);
-void Food();
-
+void Reset(struct Person r[]);
+void Food_list(struct Person r[]);
+void Food(struct Person r[]);
 
 int main(){
-	int i, j;
    	int n;
+	Reset1(a);
+	Reset2(a);
+	printf("PC방 프로그램(made by 서태영, 정승민)\n");
+	printf("\n");
    	while(1){
-   		printf("1.등록 2.자리보기 3. 자리해제 4.음식주문 5.프로그램 종료\n");
+   		printf("1.등록 2.자리보기 3. 자리해제 4.음식주문 5.음식 주문 기록 6.프로그램 종료\n");
    		n = 0;
    		scanf("%d", &n);
   			switch(n){
@@ -39,9 +40,11 @@ int main(){
 	     	    	Out(a);
 	     	    	break;
 	     	    case 4:
-	     	    	Food();
+	     	    	Food(a);
 	     	    	break;
 	      		case 5:
+	         		Food_list(a);
+	         	case 6:
 	         		return 0;
 	   }
 	}
@@ -49,7 +52,7 @@ int main(){
 
 
 void Register(struct Person r[]){
-   	int i, j, desk=0;
+   	int i, desk=0;
    	char name[20];
    	printf("자리를 입력하세요(숫자만)\n");
    	scanf("%d", &desk);
@@ -64,66 +67,138 @@ void Register(struct Person r[]){
    	}
 	
    	//기존에 자리에 사람이 있는지 비교
-   	int result = Search(desk); 
+   	int result = Search(desk, r); 
    	if(result==1){
+   		printf("이미 %d번자리에는 사람이 있습니다.\n", desk);
    		return; 
    	}
    	else{
-   		strcpy(r[k].name, name);
-   		strcpy(r[k].li, "1");
-		printf("성공적으로 등록이 되었습니다.\n");
-		k++;
+   		for(i=0; i<10; i++){
+	   		if((desk-1)==i){
+	   			strcpy(r[i].name, name);
+	   			strcpy(r[i].li, "1");
+				printf("성공적으로 등록이 되었습니다.\n");
+				return;
+			}
+			else continue;
+		}
 		return;
-   }
-	
+    }
+}
+
+void Reset1(struct Person r[]){
+	int i;
+	for(i=0; i<10; i++){
+		strcpy(r[i].li, "0");
+	}
+	return;
+}
+void Reset2(struct Person r[]){
+	int i;
+	for(i=0; i<10; i++){
+		strcpy(r[i].name, " ");
+	}
+	return;
 }
 
 void Show(struct Person r[]){
    	int j;
-//   	printf("===========================================================================\n");
-//	printf("    1      2      3      4      5      6      7      8      9      10\n");
-//	printf("---------------------------------------------------------------------------\n");
-//  	for(j=0; j<10;j++){
-//   		if(r[j].li == "0")
-//   			printf("   NULL");
-//   		else
-//   			printf("%s", r[j].name);
-//   }
-//   printf("\n===========================================================================\n");
-
 	for(j=0; j<10; j++){
 		printf("%d번 자리 : %s\n", j+1, r[j].name);
 	}
 }
 
 void Out(struct Person r[]){
-   int s;
-   printf("자리를 입력해주세요.\n");
-   scanf("%d", &s);
-   r[s-1].name == "NULL";
-   return;
+   	int s;
+   	printf("자리를 입력해주세요.\n");
+   	scanf("%d", &s);
+  	strcpy(r[s-1].name, " ");
+	strcpy(r[s-1].li, "0");
+	printf("성공적으로 해제가 되었습니다.\n");
+	return;
+		
+   
 }
 
-int Search(int desk){
-   	if(a[(desk-1)==0])
+int Search(int d, struct Person r[]){
+   	if(r[d-1].li=="1")
    		return 1;
    	else return -1;
 }
 
 
-void Food(){
+void Food(struct Person r[]){
 	do{
-		int f=0;
-		printf("1.라면 2.음료 3.과자");
-		scanf("%d", &f);
-		switch(f){
+		int food=0, s=0;
+		printf("자리 번호를 입력하세요.\n");
+		scanf("%d", &s);
+		printf("1.라면 2.음료 3.과자\n");
+		scanf("%d", &food);
+		switch(food){
 			case 1:
-				break;
+				strcpy(r[s-1].f, "1");
+				printf("라면 주문이 정상적으로 되었습니다.\n");
+				return;
 			case 2:
-				break;
+				strcpy(r[s-1].f, "2");
+				printf("음료 주문이 정상적으로 되었습니다.\n");
+				return;
 			case 3:
-				break;
+				strcpy(r[s-1].f, "3");
+				printf("과자 주문이 정상적으로 되었습니다.\n");
+				return;
 		}
 	}while(1);
 	
 }
+void Food_list(struct Person r[]){
+	int n, s, i;
+	printf("전체(1) OR 특정자리(2)\n");
+	scanf("%d", &n);
+	switch(n){
+		case 1:
+			for(i=0; i<10; i++){
+				if(r[i].f=="1"){
+					printf("%d번 자리에서는 라면을 주문했습니다.\n", i);
+					continue;
+				}
+				else if(r[i].f=="2"){
+					printf("%d번 자리에서는 음료를 주문했습니다.\n", i);
+					continue;
+				}
+				else if(r[i].f=="3"){
+					printf("%d번 자리에서는 과자를 주문했습니다.\n", i);
+					continue;
+				}
+			}
+			return;
+		case 2:
+			printf("자리를 입력하세요\n");
+			scanf("%d", &s);
+			if(r[i].f=="1"){
+				printf("%d번 자리에서는 라면을 주문했습니다.\n", i);
+				break;
+			}
+			else if(r[i].f=="2"){
+				printf("%d번 자리에서는 음료를 주문했습니다.\n", i);
+				break;
+			}
+			else if(r[i].f=="3"){
+				printf("%d번 자리에서는 과자를 주문했습니다.\n", i);
+				break;
+			}
+			else {
+				printf("음식 주문을 안했습니다.\n");	
+				break;
+			}	
+			return;	
+	}
+	
+}
+
+
+
+
+
+
+
--- test.c
+++ test.c
@@ -2,11 +2,17 @@
 #include <string.h>
 struct Person{
 	char name[10];
-	int li[10];
+	char li[10];
 };
 
 int main()
 {
-	
-	
+	struct Person n[5];
+	int i;
+	for(i=0;i<5;i++){
+		strcpy(n[i].li, "0");
+	}
+	for(i=0; i<5;i++){
+		printf("%s\n", n[i].li);
+	}
 }
